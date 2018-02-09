bf4b9c080533c2087da64aa12182b06b0f55f003
Roman Yosypenko <yosypenkoif@gmail.com>
2018-02-09 12:38:48 +0200

    wip, add popup gallery

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,17 @@ $('.js-rating').raty({
   score: 3,
   path:  "/assets/"
 });
+
+$('.js-popup-link').magnificPopup({
+  type: 'image',
+  gallery: {
+    enabled: true
+  },
+  image: {
+    titleSrc: function(item) {
+      var title = item.el.attr('title'),
+          text = item.el.data('description');
+			return  '<span>' + title + '</span>' + '<div>' + text + '<div>';
+		}
+  }
+});
